759f5bce859c5d8e4d6d509e41389719be589c78
Rajib Chandra Das <prorajib6@gmail.com>
2024-04-25 22:01:41 +0600

    create user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// delete old model if exists
+const userModel = mongoose.model('user');
+mongoose.deleteModel(userModel.modelName);
+
+// create new model
+module.exports = mongoose.model('users', userSchema);
