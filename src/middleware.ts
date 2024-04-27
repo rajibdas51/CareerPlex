4a385c3c880e8e72772f7ee647d93d914c2f9bf8
Rajib Chandra Das <prorajib6@gmail.com>
2024-04-27 18:25:44 +0600

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  try {
+    const isPublicPage =
+      request.nextUrl.pathname === '/login' ||
+      request.nextUrl.pathname === '/register';
+    // if there is no token and the page is not public, redirect to login.
+    const token = request.cookies.get('token');
+    if (!token && !isPublicPage) {
+      return NextResponse.redirect(new URL('/login', request.nextUrl));
+    }
+    // if there is a token and the page is public, then rediret to home.
+    if (token && isPublicPage) {
+      return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
+
+export const config = {
+  matcher: ['/', '/login', '/register'],
+};
