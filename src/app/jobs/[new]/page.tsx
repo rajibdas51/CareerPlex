b78bac40ff1fce5b76b92f6dde13cdd265081986
Rajib Chandra Das <prorajib6@gmail.com>
2024-05-05 16:28:22 +0600

    add create job page

--- src/app/jobs/[new]/page.tsx
+++ src/app/jobs/[new]/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+import PageTitle from '@/components/PageTitle';
+import { Button } from 'antd';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 function NewJob() {
-  return <div>New job</div>;
+  const router = useRouter();
+  return (
+    <div className='flex justify-between items-center pt-3'>
+      <PageTitle title='Post New Job' />
+      <Button onClick={() => router.back()} type='primary'>
+        Back
+      </Button>
+    </div>
+  );
 }
 
 export default NewJob;
