0501146b1fa50f52942d3bea7aabc16be2f586b8
Rajib Chandra Das <prorajib6@gmail.com>
2024-05-05 16:24:51 +0600

    add jobs page

---
+++ src/app/jobs/[new]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewJob() {
+  return <div>New job</div>;
+}
+
+export default NewJob;
---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import PageTitle from '@/components/PageTitle';
+import { Button } from 'antd';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+function Jobs() {
+  const router = useRouter();
+  return (
+    <div className='flex justify-between py-3'>
+      <PageTitle title='Jobs' />
+      <Button onClick={() => router.push('/jobs/new')} type='primary'>
+        Create Job
+      </Button>
+    </div>
+  );
+}
+
+export default Jobs;
