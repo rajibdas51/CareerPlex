cee616a0dc33d6ad56c3332db01a44120cebd090
Rajib <prorajib6@gmail.com>
2025-02-18 19:57:34 +0600

    create put api to update profile

--- src/app/api/users/[userid]/route.ts
+++ src/app/api/users/[userid]/route.ts
@@ -22,3 +22,37 @@ export async function GET(request: NextRequest, { params }: any) {
     );
   }
 }
+
+export async function PUT(request: NextRequest) {
+  try {
+    // validate JWT and get user ID
+    const userId = await validateJWT(request);
+    if (!userId) {
+      return NextResponse.json({ message: 'Unauthorized!' }, { status: 401 });
+    }
+
+    // parse the request
+    const body = await request.json();
+
+    // update the user
+    const updatedUser = await User.findByIdAndUpdate(userId, body, {
+      new: true,
+      runValidators: true,
+    }).select('-password');
+
+    if (!updatedUser) {
+      return NextResponse.json({ message: 'User not found!' }, { status: 404 });
+    }
+    return NextResponse.json({
+      message: 'Profile updated Successfully!',
+      data: updatedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message || 'Something went wrong!',
+      },
+      { status: 500 }
+    );
+  }
+}
